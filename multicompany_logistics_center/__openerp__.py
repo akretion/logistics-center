272dbef5fcdb3a1222ac535ec90460989eb3253f
david.beal@akretion.com <david.beal@akretion.com>
2014-10-03 00:27:32 +0200

    [ADD] multicpny logistics center module

---
+++ multicompany_logistics_center/__openerp__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   Copyright (C) 2014-TODAY Akretion <http://www.akretion.com>.
+#     All Rights Reserved
+#     @author David BEAL <david.beal@akretion.com>
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Multicompany logistics center',
+    'version': '0.8',
+    'category': 'Warehouse',
+    'sequence': 10,
+    'summary': "Multicompany backend logistics center ",
+    'description': """
+Multicompany logistics center
+==============================
+
+Description
+-----------
+
+Allow to use the same logistics backend for several companies.
+
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': [
+        'connector_logistic_center',
+    ],
+    'external_dependencies': {
+        'Python': [],
+    },
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'images': [
+    ],
+    'css': [
+    ],
+    'js': [
+    ],
+    'qweb': [
+    ],
+}
---
+++ multicompany_logistics_center/backend.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#  licence AGPL version 3 or later
+#  see licence in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
+#  Copyright (C) 2014 Akretion (http://www.akretion.com).
+#  @author David BEAL <david.beal@akretion.com>
+#
+##############################################################################
+
+
+from openerp.osv import orm, fields
+#from openerp.tools.translate import _
+
+
+class LogisticBackend(orm.Model):
+    _inherit = 'logistic.backend'
+
+    _columns = {
+        'warehouse_id': fields.property(
+            'stock.warehouse',
+            relation='stock.warehouse',
+            string='Warehouse',
+            type='many2one',
+            required=True,
+            ondelete="cascade",
+            view_load=True,
+            help="Warehouse of the logistics center"),
+    }
