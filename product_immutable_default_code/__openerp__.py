e7cc150a6087d6db2588271f9ce377b2aea788e1
David Beal <david.beal@akretion.com>
2016-01-19 17:49:21 +0100

    [ADD] module product_immutable_code

---
+++ product_immutable_default_code/__openerp__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+# © 2015 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product Default Code Unwritable',
+    'version': '0.5',
+    'category': 'product',
+    'sequence': 10,
+    'description': """
+Make default code unwritable""",
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'product_view.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
---
+++ product_immutable_default_code/product.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+# © 2015 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.osv import orm, fields
+from openerp.tools.translate import _
+
+
+class ProductProduct(orm.Model):
+    _inherit = 'product.product'
+
+    _columns = {
+        'sent_to_logistics': fields.boolean('Sent To Logistics'),
+    }
+
+    def write(self, cr, uid, ids, vals, context=None):
+        if 'defaut_code' in vals:
+            immutable_code_products = []
+            for product in self.browse(cr, uid, ids, context=context):
+                if product.sent_to_logistics:
+                    immutable_code_products.append(
+                        (product.name, product.id))
+            if immutable_code_products:
+                raise orm.except_orm(
+                    _('Internal Reference'),
+                    _(self.default_code_alert()) % immutable_code_products)
+        return super(ProductProduct, self).write(
+            cr, uid, ids, vals, context=context)
+
+    def default_code_alert(self):
+        return _("Internal Reference of these products '%s' "
+                 "can't be changed because it have been exchanged with "
+                 "external partners.")
